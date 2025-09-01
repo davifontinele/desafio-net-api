44224538691aaeda181dc18b26eeaa7981dea40e
Davi <davifontineledevi@gmail.com>
2025-09-01 15:37:14 -0300

    Finalizando desafio

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -5,11 +5,14 @@ namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
-        {
-            
-        }
-
         public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=MinhaBase;User=root;Password=1234;";
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,46 @@
+
 using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace desaio_api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseMySql(
+    builder.Configuration.GetConnectionString("ConexaoPadrao"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+));
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
